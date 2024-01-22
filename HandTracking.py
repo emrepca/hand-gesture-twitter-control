7c9768195d15779437a3eddb8d48f826eed78139
emrepca <emrepaca3@gmail.com>
2024-01-23 02:29:45 +0300

    'retweet' movement is defined

--- HandTracking.py
+++ HandTracking.py
@@ -7,8 +7,13 @@ hand_processor = HandProcessor()
 
 while True:
     success, img = camera.read()
-    processed_img = hand_processor.like_func(img)
-    cv.imshow("Camera", processed_img)
+
+    processed_img_like = hand_processor.like_func(img)
+    cv.imshow("Like processing", processed_img_like)
+
+    processed_img_retweet = hand_processor.retweet_func(img)
+    #cv.imshow("Retweet processing", processed_img_retweet)
+
 
     if cv.waitKey(1) & 0xFF == 27:
         break
--- hand_processing.py
+++ hand_processing.py
@@ -6,6 +6,7 @@ class HandProcessor:
         self.mpHands = mediapipe.solutions.hands
         self.hands = self.mpHands.Hands(max_num_hands=2)
         self.mpDraw = mediapipe.solutions.drawing_utils
+
     def like_func(self, img):
         imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
         hlms = self.hands.process(imgRGB)
@@ -37,3 +38,42 @@ class HandProcessor:
                 self.mpDraw.draw_landmarks(img, handlandmarks, self.mpHands.HAND_CONNECTIONS)
 
         return img
+
+
+    def retweet_func(self, img):
+        imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        hlms = self.hands.process(imgRGB)
+        height, width, _ = img.shape  # _ : channel (we do not use)
+
+        if hlms.multi_hand_landmarks:
+            for handlandmarks in hlms.multi_hand_landmarks:
+                for fingerNum, landmark in enumerate(handlandmarks.landmark):
+                    positionX, positionY = int(landmark.x * width), int(landmark.y * height)
+
+                    if handlandmarks.landmark[12].y > handlandmarks.landmark[9].y:
+                        break
+
+                    if handlandmarks.landmark[16].y > handlandmarks.landmark[13].y:
+                        break
+
+                    if handlandmarks.landmark[20].y > handlandmarks.landmark[17].y:
+                        break
+
+                    if fingerNum == 4 or fingerNum == 8:
+
+                        thumb_tip = handlandmarks.landmark[4]
+                        index_tip = handlandmarks.landmark[8]
+
+                        thumb_x, thumb_y = int(thumb_tip.x * width), int(thumb_tip.y * height)
+                        index_x, index_y = int(index_tip.x * width), int(index_tip.y * height)
+
+                        distance = ((thumb_x - index_x) ** 2 + (thumb_y - index_y) ** 2) ** 0.5
+                        #print(distance)
+
+                        if distance < 20:
+                            print("Retweet!")
+
+                #self.mpDraw.draw_landmarks(img, handlandmarks, self.mpHands.HAND_CONNECTIONS)
+
+        return img
+
