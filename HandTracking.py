abd6cf27ed46aa7353693171a42dfcfc3faa1f27
emrepca <emrepaca3@gmail.com>
2024-05-21 13:50:43 +0300

    new movements have been added, the code has been edited

--- HandTracking.py
+++ HandTracking.py
@@ -5,16 +5,20 @@ from hand_processing import HandProcessor
 camera = cv.VideoCapture(0)
 hand_processor = HandProcessor()
 
-while True:
+while camera.isOpened():
     success, img = camera.read()
 
-    processed_img_like = hand_processor.like_func(img)
-    cv.imshow("Like processing", processed_img_like)
+    rgbImage = hand_processor.bgr2RGB(img)
+    hlms = hand_processor.handLandMarkModel(rgbImage)
 
-    processed_img_retweet = hand_processor.retweet_func(img)
-    #cv.imshow("Retweet processing", processed_img_retweet)
+    processed_like_func = hand_processor.like_func(hlms, rgbImage)
+    processed_retweet_func = hand_processor.retweet_func(hlms, rgbImage)
+    processed_scroll_down_func = hand_processor.scroll_down_func(hlms, rgbImage)
+    processed_scroll_up_func = hand_processor.scroll_up_func(hlms, rgbImage)
+    processed_bookmark_func = hand_processor.bookmark_func(hlms, rgbImage)
+    processed_empty_func = hand_processor.empty_func(hlms, rgbImage)
 
-    processed_img_scroll = hand_processor.scroll_func(img)
+    cv.imshow("WebCam", processed_like_func)
 
     if cv.waitKey(1) & 0xFF == 27:
         break
--- hand_processing.py
+++ hand_processing.py
@@ -1,65 +1,55 @@
 import cv2 as cv
 import mediapipe
+import keyboard
+import time
 
 
 class HandProcessor:
     def __init__(self):
         self.mpHands = mediapipe.solutions.hands
-        self.hands = self.mpHands.Hands(max_num_hands=2)
+        self.hands = self.mpHands.Hands(max_num_hands=1)
         self.mpDraw = mediapipe.solutions.drawing_utils
 
-    def like_func(self, img):
+    def bgr2RGB(self, img):
         imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        return imgRGB
+
+    def handLandMarkModel(self, imgRGB):
         hlms = self.hands.process(imgRGB)
-        height, width, _ = img.shape  # _ : channel (we do not use)
+        return hlms
+
+    def like_func(self, hlms, imgRGB):
+        height, width, _ = imgRGB.shape  # _ : channel (we do not use)
 
         if hlms.multi_hand_landmarks:
             for handlandmarks in hlms.multi_hand_landmarks:
                 for fingerNum, landmark in enumerate(handlandmarks.landmark):
                     positionX, positionY = int(landmark.x * width), int(landmark.y * height)
 
-                    if fingerNum > 4 and landmark.y < handlandmarks.landmark[2].y:
-                        break
+                    if (fingerNum < 4 and landmark.y > handlandmarks.landmark[2].y and
+                        handlandmarks.landmark[5].x > handlandmarks.landmark[8].x and
+                        handlandmarks.landmark[9].x > handlandmarks.landmark[12].x and
+                        handlandmarks.landmark [13].x > handlandmarks.landmark[16].x and
+                        handlandmarks.landmark[17].x > handlandmarks.landmark[20].x and
+                        handlandmarks.landmark[20].y > handlandmarks.landmark[2].y):
+                        print("Like")
 
-                    if fingerNum == 5 and landmark.x < handlandmarks.landmark[8].x:
-                        break
+                self.mpDraw.draw_landmarks(imgRGB, handlandmarks, self.mpHands.HAND_CONNECTIONS)
 
-                    if fingerNum == 9 and landmark.x < handlandmarks.landmark[12].x:
-                        break
+        return imgRGB
 
-                    if fingerNum == 13 and landmark.x < handlandmarks.landmark[16].x:
-                        break
-
-                    if fingerNum == 17 and landmark.x < handlandmarks.landmark[20].x:
-                        break
-
-                    if fingerNum == 20 and landmark.y > handlandmarks.landmark[2].y:
-                        print("Like!")
-
-                self.mpDraw.draw_landmarks(img, handlandmarks, self.mpHands.HAND_CONNECTIONS)
-
-        return img
-
-    def retweet_func(self, img):
-        imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-        hlms = self.hands.process(imgRGB)
-        height, width, _ = img.shape  # _ : channel (we do not use)
+    def retweet_func(self, hlms, imgRGB):
+        height, width, _ = imgRGB.shape  # _ : channel (we do not use)
 
         if hlms.multi_hand_landmarks:
             for handlandmarks in hlms.multi_hand_landmarks:
                 for fingerNum, landmark in enumerate(handlandmarks.landmark):
                     positionX, positionY = int(landmark.x * width), int(landmark.y * height)
 
-                    if handlandmarks.landmark[12].y > handlandmarks.landmark[9].y:
-                        break
-
-                    if handlandmarks.landmark[16].y > handlandmarks.landmark[13].y:
-                        break
-
-                    if handlandmarks.landmark[20].y > handlandmarks.landmark[17].y:
-                        break
-
-                    if fingerNum == 4 or fingerNum == 8:
+                    if (handlandmarks.landmark[12].y < handlandmarks.landmark[11].y and
+                        handlandmarks.landmark[16].y < handlandmarks.landmark[15].y and
+                        handlandmarks.landmark[20].y < handlandmarks.landmark[19].y and
+                        fingerNum == 4 or fingerNum == 8):
 
                         thumb_tip = handlandmarks.landmark[4]
                         index_tip = handlandmarks.landmark[8]
@@ -68,42 +58,93 @@ class HandProcessor:
                         index_x, index_y = int(index_tip.x * width), int(index_tip.y * height)
 
                         distance = ((thumb_x - index_x) ** 2 + (thumb_y - index_y) ** 2) ** 0.5
-                        # print(distance)
+                        #print(distance)
 
-                        if distance < 20:
+                        if distance < 30:
                             print("Retweet!")
 
                 # self.mpDraw.draw_landmarks(img, handlandmarks, self.mpHands.HAND_CONNECTIONS)
 
-        return img
+        #return imgRGB
 
-    def scroll_func(self, img):
-        imgRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-        hlms = self.hands.process(imgRGB)
-        height, width, _ = img.shape
+
+    def scroll_down_func(self, hlms, imgRGB):
+        height, width, _ = imgRGB.shape
 
         if hlms.multi_hand_landmarks:
             for handlandmarks in hlms.multi_hand_landmarks:
                 for fingerNum, landmark in enumerate(handlandmarks.landmark):
                     positionX, positionY = int(landmark.x * width), int(landmark.y * height)
 
-                    if handlandmarks.landmark[12].y < handlandmarks.landmark[9].y:
-                        break
+                    if (handlandmarks.landmark[12].y > handlandmarks.landmark[9].y and
+                        handlandmarks.landmark[16].y > handlandmarks.landmark[13].y and
+                        handlandmarks.landmark[20].y > handlandmarks.landmark[17].y and
+                        handlandmarks.landmark[4].x < handlandmarks.landmark[5].x and
+                        handlandmarks.landmark[8].y < handlandmarks.landmark[5].y):
+                        print("Scroll Down")
 
-                    if handlandmarks.landmark[16].y < handlandmarks.landmark[13].y:
-                        break
+            #return imgRGB
 
-                    if handlandmarks.landmark[20].y < handlandmarks.landmark[17].y:
-                        break
 
-                    if handlandmarks.landmark[4].x > handlandmarks.landmark[5].x:
-                        break
+    def scroll_up_func(self, hlms, imgRGB): #sorunlu
+        height, width, _ = imgRGB.shape
 
-                    if handlandmarks.landmark[8].y < handlandmarks.landmark[5].y:
+        if hlms.multi_hand_landmarks:
+            for handlandmarks in hlms.multi_hand_landmarks:
+                for fingerNum, landmark in enumerate(handlandmarks.landmark):
+                    positionX, positionY = int(landmark.x * width), int(landmark.y * height)
 
-                        if handlandmarks.landmark[8].y * 480 > 240:  # 480: camera height
-                            print("Scroll Up")
-                        else:
-                            print("Scroll Down")
+                    if (handlandmarks.landmark[8].y > handlandmarks.landmark[5].y and
+                        handlandmarks.landmark[12].y < handlandmarks.landmark[9].y and
+                        handlandmarks.landmark[16].y < handlandmarks.landmark[13].y and
+                        handlandmarks.landmark[20].y < handlandmarks.landmark[17].y and
+                        handlandmarks.landmark[4].x < handlandmarks.landmark[5].x and
+                        handlandmarks.landmark[4].y > handlandmarks.landmark[0].y):
+                        print("Scroll Up")
 
-            return img
+            #return imgRGB
+
+
+
+    def bookmark_func(self, hlms, imgRGB):
+        height, width, _ = imgRGB.shape
+
+        if hlms.multi_hand_landmarks:
+            for handlandmarks in hlms.multi_hand_landmarks:
+                for fingerNum, landmark in enumerate(handlandmarks.landmark):
+                    positionX, positionY = int(landmark.x * width), int(landmark.y * height)
+
+                    if (handlandmarks.landmark[8].y > handlandmarks.landmark[7].y and
+                        handlandmarks.landmark[12].y > handlandmarks.landmark[11].y and
+                        handlandmarks.landmark[16].y > handlandmarks.landmark[15].y and
+                        handlandmarks.landmark[20].y > handlandmarks.landmark[19].y and
+                        handlandmarks.landmark[4].x > handlandmarks.landmark[1].x):
+                        print("Bookmark!")
+
+
+
+    def empty_func(self, hlms, imgRGB):
+
+        height, width, _ = imgRGB.shape
+
+        if hlms.multi_hand_landmarks:
+            for handlandmarks in hlms.multi_hand_landmarks:
+                for fingerNum, landmark in enumerate(handlandmarks.landmark):
+                    positionX, positionY = int(landmark.x * width), int(landmark.y * height)
+
+                    thumb_tip = handlandmarks.landmark[4]
+                    index_tip = handlandmarks.landmark[8]
+
+                    thumb_x, thumb_y = int(thumb_tip.x * width), int(thumb_tip.y * height)
+                    index_x, index_y = int(index_tip.x * width), int(index_tip.y * height)
+
+                    distance = ((thumb_x - index_x) ** 2 + (thumb_y - index_y) ** 2) ** 0.5
+
+                    if (handlandmarks.landmark[4].y < handlandmarks.landmark[3].y and
+                        handlandmarks.landmark[8].y < handlandmarks.landmark[7].y and
+                        handlandmarks.landmark[12].y < handlandmarks.landmark[11].y and
+                        handlandmarks.landmark[16].y < handlandmarks.landmark[15].y and
+                        handlandmarks.landmark[20].y < handlandmarks.landmark[19].y and
+                        handlandmarks.landmark[8].y < handlandmarks.landmark[4].y and
+                        distance > 120):
+                        print("Empty hand!")
